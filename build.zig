2c7d5148231fb6ccb38641bd3f530d3862629492
charlcoal <charley.reischer@gmail.com>
2025-12-12 09:18:32 -0500

    added license preamble to build files

--- build.zig
+++ build.zig
@@ -1,3 +1,18 @@
+// BROT - A fast mandelbrot set explorer Copyright (C) 2025  Charles Reischer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 const std = @import("std");
 const cimgui = @import("cimgui_zig");
 const Resources = struct {
@@ -34,7 +49,7 @@ pub fn build(b: *std.Build) !void {
 
     exe.linkLibrary(glfw.artifact("glfw"));
     exe.linkLibrary(cimgui_dep.artifact("cimgui"));
-    exe.linkSystemLibrary("vulkan");
+    exe.root_module.linkSystemLibrary("vulkan", .{});
 
     b.installArtifact(exe);
 
