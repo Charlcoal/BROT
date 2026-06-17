e69ef89d3ac1122e04278c0023244414c95d4942
charlcoal <charley.reischer@gmail.com>
2026-06-17 09:36:37 -0400

    improved gui

--- src/imgui.zig
+++ src/imgui.zig
@@ -76,3 +76,64 @@ pub fn init() void {
     };
     _ = c.cImGui_ImplVulkan_Init(&info);
 }
+
+pub const ScalarInputOptions = struct {
+    doubler_divider: bool = false,
+    h_align: ?f32 = null,
+};
+pub fn scalarInput(comptime name: [:0]const u8, desc: ?[:0]const u8, cur_val: anytype, options: ScalarInputOptions) ?@TypeOf(cur_val) {
+    c.ImGui_AlignTextToFramePadding();
+
+    c.ImGui_TextUnformatted(name);
+    if (options.h_align) |width| {
+        c.ImGui_SameLineEx(width, 5.0);
+    } else {
+        c.ImGui_SameLine();
+    }
+
+    if (desc) |d| {
+        toolTip(d);
+    }
+
+    var new_val = cur_val;
+    var updated: bool = false;
+    if (options.doubler_divider) {
+        if (c.ImGui_Button("-##" ++ name)) {
+            new_val = @divTrunc(new_val, 2);
+            updated = true;
+        }
+        c.ImGui_SameLineEx(0, 5.0);
+        if (c.ImGui_Button("+##" ++ name)) {
+            new_val *= 2;
+            updated = true;
+        }
+        c.ImGui_SameLineEx(0, 5.0);
+    }
+
+    const imgui_type = switch (@TypeOf(cur_val)) {
+        f32 => c.ImGuiDataType_Float,
+        f64 => c.ImGuiDataType_Double,
+        i8 => c.ImGuiDataType_S8,
+        u8 => c.ImGuiDataType_U8,
+        i16 => c.ImGuiDataType_S16,
+        u16 => c.ImGuiDataType_U16,
+        i32 => c.ImGuiDataType_S32,
+        u32 => c.ImGuiDataType_U32,
+        i64 => c.ImGuiDataType_S64,
+        u64 => c.ImGuiDataType_U64,
+        else => @compileError("invalid type in scalarInput!"),
+    };
+    if (c.ImGui_InputScalar("##" ++ name, imgui_type, &new_val)) updated = true;
+
+    if (updated) return new_val;
+    return null;
+}
+
+pub fn toolTip(desc: [:0]const u8) void {
+    if (c.ImGui_IsItemHovered(c.ImGuiHoveredFlags_DelayNormal) and c.ImGui_BeginTooltip()) {
+        c.ImGui_PushTextWrapPos(c.ImGui_GetFontSize() * 35.0);
+        c.ImGui_TextUnformatted(desc);
+        c.ImGui_PopTextWrapPos();
+        c.ImGui_EndTooltip();
+    }
+}
