084a1d2beb02818252c4875c54a2d8d5be72010d
charlcoal <charley.reischer@gmail.com>
2026-06-26 08:40:42 -0400

    fixed panning for real this time and changed defaults slightly

--- src/main_loop.zig
+++ src/main_loop.zig
@@ -374,9 +374,9 @@ fn renderedBufferResolve(io: std.Io) !void {
             offset.x += @as(f64, @floatFromInt(common.remap_x)); // * scale;
             offset.y += @as(f64, @floatFromInt(common.remap_y)); // * scale;
 
-            if (offset.y * @as(f64, @floatFromInt(common.renderPatchSize(common.max_res_scale_exponent))) >=
+            if (@abs(offset.y) * @as(f64, @floatFromInt(common.renderPatchSize(common.max_res_scale_exponent))) >=
                 @as(f64, @floatFromInt(window.height)) / 2.0) common.background_needs_render = true;
-            if (offset.x * @as(f64, @floatFromInt(common.renderPatchSize(common.max_res_scale_exponent))) >=
+            if (@abs(offset.x) * @as(f64, @floatFromInt(common.renderPatchSize(common.max_res_scale_exponent))) >=
                 @as(f64, @floatFromInt(window.width)) / 2.0) common.background_needs_render = true;
         }
 
